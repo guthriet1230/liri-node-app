e46eef1eadc61331cd0c525900dc3b6134e02d4e
Tyler G <guthriet1230@gmail.com>
2018-11-07 23:26:19 -0600

    adjustments

--- liri.js
+++ liri.js
@@ -8,23 +8,17 @@ let Spotify = require("node-spotify-api");
 let moment = require("moment");
 let fs = require("fs");
 let ask = process.argv[2];
-let input = process.argv[3];
+let input = process.argv.slice(3).join(" ");
 
 //////////////////////////////////////////////////////////////////*
 //*  ONE - Bands in Town
 //////////////////////////////////////////////////////////////////*
-let concertSearch = function() {
-  //! Making sure that all arguments after process.argv3 are included in the band name search
-    // for (i = 3; i < process.argv.length; i++) {
-    //   input += process.argv[i] + "";
-    //   console.log(input);
-    // }
-
+let concertSearch = function(searchTerm) {
   //* The Search
   //* -------------------------------------------------------------
   request(
     "https://rest.bandsintown.com/artists/" +
-      input +
+      searchTerm +
       "/events?app_id=codingbootcamp",
     function(error, response, body) {
       if (!error && response.statusCode === 200) {
@@ -56,19 +50,13 @@ let concertSearch = function() {
 //////////////////////////////////////////////////////////////////*
 //* TWO - SPOTIFY
 //////////////////////////////////////////////////////////////////*
-let spotifySearch = function() {
+let spotifySearch = function(searchTerm) {
   let spotify = new Spotify(keys.spotify);
   //console.log(keys.spotify);
 
-  //! Making sure that all arguments after process.argv3 are included in the band name search
-  //   for (i = 3; i < process.argv.length; i++) {
-  //     input += process.argv[i] + " ";
-  //     console.log(input);
-  //   }
-
   //* The Search
   //* -------------------------------------------------------------
-  spotify.search({ type: "track", query: input }, function(err, data) {
+  spotify.search({ type: "track", query: searchTerm }, function(err, data) {
     if (err) {
       return console.log("Error occurred: " + err);
     }
@@ -86,45 +74,34 @@ let spotifySearch = function() {
 //////////////////////////////////////////////////////////////////*
 //* THREE - OMDB
 //////////////////////////////////////////////////////////////////*
-let movieSearch = function() {
-  //! Making sure that all arguments after process.argv3 are included in the band name search
-  //   for (i = 3; i < process.argv.length; i++) {
-  //     input += process.argv[i] + " ";
-  //     console.log(input);
-  //   }
-
-  //! If nothing is found, provide 'Mr Nobody'
-  //   if (data === "") {
-  //     whichMovie = "Mr+Nobody";
-  //   } else {
-
-  //* The Search
-  //* -------------------------------------------------------------
+let movieSearch = function(searchTerm) {
+  // //* The Search
+  // //* -------------------------------------------------------------
   request(
-    "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy",
+    "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy",
     function(error, response, body) {
       if (!error && response.statusCode === 200) {
         // console.log(body);
-      }
 
-      //* Displaying all the information
-      //* -------------------------------------------------------------
-      console.log("--------------------");
-      console.log("Title: " + JSON.parse(body).Title);
-      console.log("Release Year: " + JSON.parse(body).Year);
-      console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
-      console.log(
-        "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value
-      );
-      console.log("Country: " + JSON.parse(body).Country);
-      console.log("Lanugage: " + JSON.parse(body).Language);
-      console.log("Plot: " + JSON.parse(body).Plot);
-      console.log("Actors: " + JSON.parse(body).Actors);
-      console.log("--------------------");
+        //     //* Displaying all the information
+        //     //* -------------------------------------------------------------
+        console.log("--------------------");
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log(
+          "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value
+        );
+        console.log("Country: " + JSON.parse(body).Country);
+        console.log("Lanugage: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+        console.log("--------------------");
+      }
     }
   );
 };
-// }
+
 //////////////////////////////////////////////////////////////////*
 //* FOUR - Do what it says
 //////////////////////////////////////////////////////////////////*
@@ -134,24 +111,18 @@ let doItSearch = function() {
       return console.log(error);
     }
 
-    // We will then print the contents of data
-    // console.log(data);
-
-    // Then split it by commas (to make it more readable)
     var dataArr = data.split(",");
     let ask = dataArr[0];
     let input = dataArr[1];
 
-    console.log(ask + " " + input);
-
     if (ask === "concert-this") {
-      concertSearch();
+      concertSearch(input);
     }
     if (ask === "movie-this") {
-      movieSearch();
+      movieSearch(input);
     }
     if (ask === "spotify-this-song") {
-      spotifySearch();
+      spotifySearch(input);
     }
   });
 };
@@ -161,14 +132,14 @@ let doItSearch = function() {
 //////////////////////////////////////////////////////////////////*
 
 if (ask === "concert-this") {
-  concertSearch();
+  concertSearch(input);
 }
 if (ask === "movie-this") {
-  movieSearch();
+  movieSearch(input);
 }
 if (ask === "spotify-this-song") {
-  spotifySearch();
+  spotifySearch(input);
 }
 if (ask === "do-what-it-says") {
-  doItSearch();
+  doItSearch(input);
 }
